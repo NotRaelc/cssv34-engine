07e7fb1bd5825b6c3925fe35564419d938d42b24
entityname <mcetverov26@gmail.com>
2026-07-17 14:17:04 +0400

    fixed "Unregistered message 'Rumble'" when starting listen server

--- src/game/server/player.cpp
+++ src/game/server/player.cpp
@@ -1615,8 +1615,6 @@ void CBasePlayer::Event_Killed( const CTakeDamageInfo &info )
 
 	gamestats->Event_PlayerKilled( this, info );
 
-	RumbleEffect( RUMBLE_STOP_ALL, 0, RUMBLE_FLAGS_NONE );
-
 	ClearUseEntity();
 	
 	// this client isn't going to be thinking for a while, so reset the sound until they respawn
@@ -4890,8 +4888,6 @@ void CBasePlayer::Spawn( void )
 		gameeventmanager->FireEvent( event );
 	}
 
-	RumbleEffect( RUMBLE_STOP_ALL, 0, RUMBLE_FLAGS_NONE );
-
 	// Calculate this immediately
 	m_nVehicleViewSavedFrame = 0;
 }
@@ -4901,8 +4897,6 @@ void CBasePlayer::Activate( void )
 	BaseClass::Activate();
 
 	AimTarget_ForceRepopulateList();
-
-	RumbleEffect( RUMBLE_STOP_ALL, 0, RUMBLE_FLAGS_NONE );
 
 	// Reset the analog bias. If the player is in a vehicle when the game
 	// reloads, it will autosense and apply the correct bias.
@@ -5402,9 +5396,6 @@ void CBasePlayer::LeaveVehicle( const Vector &vecExitPoint, const QAngle &vecExi
 			ShowCrosshair( true );
 		}
 	}
-
-	// Just cut all of the rumble effects. 
-	RumbleEffect( RUMBLE_STOP_ALL, 0, RUMBLE_FLAGS_NONE );
 }
 
 
@@ -6647,21 +6638,6 @@ void CBasePlayer::UpdateClientData( void )
 
 	// Let any global rules update the HUD, too
 	g_pGameRules->UpdateClientData( this );
-}
-
-void CBasePlayer::RumbleEffect( unsigned char index, unsigned char rumbleData, unsigned char rumbleFlags )
-{
-	if( !IsAlive() )
-		return;
-
-	CSingleUserRecipientFilter filter( this );
-	filter.MakeReliable();
-
-	UserMessageBegin( filter, "Rumble" );
-	WRITE_BYTE( index );
-	WRITE_BYTE( rumbleData );
-	WRITE_BYTE( rumbleFlags	);
-	MessageEnd();
 }
 
 void CBasePlayer::EnableControl(bool fControl)
